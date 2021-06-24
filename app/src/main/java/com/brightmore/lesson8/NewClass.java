a888101195deca72b9bff31ac2176a3f492d241c
brightmore32 <caleb_brightmore@mail.ru>
2021-06-24 08:05:24 +0300

    add new class

---
+++ app/src/main/java/com/brightmore/lesson8/NewClass.java
@@ -0,0 +1,13 @@
+package com.brightmore.lesson8;
+
+public class  NewClass {
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    int id;
+}
